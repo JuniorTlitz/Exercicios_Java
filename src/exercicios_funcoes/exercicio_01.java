1860dad5af4a72877a81ea4d521b6f45eadea108
Tarcizio <tarcizio.junior2318@outlook.com>
2023-03-07 17:28:03 -0300

    exercicio_01 refeito

--- src/exercicios_funcoes/exercicio_01.java
+++ src/exercicios_funcoes/exercicio_01.java
@@ -3,14 +3,13 @@ package exercicios_funcoes;
 public class exercicio_01 {
     public static void main(String[] args) {
         // Faça uma função que recebe por parâmetro o raio de uma esfera e calcula o seu volume (v = 4/3.P .R3).
-        System.out.println("O volume da esfera é de: " + calcVolumeEsfera(5));
+        double raio = 5;
+        double volume = calcularVolumeEsfera(raio);
+        System.out.printf("O raio é %.2f e o volume é %.2f", raio, volume);
     }
 
-    public static double calcVolumeEsfera(double raio){
-        double expoente = Math.pow(raio, 3);
-        int div = 4 / 3;
-        double PI = 3.14;
-        return div * PI * expoente;
+    public static double calcularVolumeEsfera(double raio) {
+        return (4.0/3.0) * Math.PI * Math.pow(raio, 3);
     }
 
 }
